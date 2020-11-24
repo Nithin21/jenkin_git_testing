9b96f07d9dd8a5c9f83a0e896036f4188137db19
Nithin21 <55268308+Nithin21@users.noreply.github.com>
2020-11-24 10:46:51 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,9 @@
 print("Hello World"); #print hello world
 print("wassup hook"); #testing webhook with jenkins
 #last test didn't work
+def sup():
+  a = 0;
+  while a < 10:
+    print(a+2);
+  
+ sup()
